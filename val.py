9353bf6bfcb38cc0fa84f8f1bdc4179e5f525fd9
hai7419 <wangzhenxia18@163.com>
2023-05-15 13:36:45 +0800

    ddsvds

--- val.py
+++ val.py
@@ -103,98 +103,89 @@ def validate(
     return (mp,mr,map50,map,*(loss.cpu() / len(dataloader))),maps
 
 
-def ap_per_class(
-        tp, 
-        conf, 
-        pred_cls, 
-        target_cls,
-        names = None, 
-        eps=1e-16 
-        ):
+
+
+
+
+
+def ap_per_class(tp, conf, pred_cls, target_cls,  names=(), eps=1e-16):
+    """ Compute the average precision, given the recall and precision curves.
+    Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:  True positives (nparray, nx1 or nx10).
+        conf:  Objectness value from 0-1 (nparray).
+        pred_cls:  Predicted object classes (nparray).
+        target_cls:  True object classes (nparray).
+        plot:  Plot precision-recall curve at mAP@0.5
+        save_dir:  Plot save directory
+    # Returns
+        The average precision as computed in py-faster-rcnn.
     """
-        Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
 
-        input   tp          []
-                conf        []
-                pred_cls    []
-                target_cls  
-                names
-                eps             zuixiaozhi
-        output  tp
-                fp
-                p
-                r
-                f1
-                ap
-
-
-    """
+    # Sort by objectness
     i = np.argsort(-conf)
-    tp, conf, pred_cls = tp[i], conf[i],pred_cls[i]
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
 
-    unique_classes,nt = np.unique(target_cls,return_counts=True)
-    nc = unique_classes.shape[0]
+    # Find unique classes
+    unique_classes, nt = np.unique(target_cls, return_counts=True)
+    nc = unique_classes.shape[0]  # number of classes, number of detections
 
-    px, py = np.linspace(0,1,1000),[]
-    ap,p,r = np.zeros((nc,tp.shape[1])),np.zeros((nc,1000)),np.zeros((nc,1000))
-    for ci,c in enumerate(unique_classes):
+    # Create Precision-Recall curve and compute AP for each class
+    px, py = np.linspace(0, 1, 1000), []  # for plotting
+    ap, p, r = np.zeros((nc, tp.shape[1])), np.zeros((nc, 1000)), np.zeros((nc, 1000))
+    for ci, c in enumerate(unique_classes):
         i = pred_cls == c
-        n_l = nt[ci]
-        n_p = i.sum()
-        if n_p == 0 or n_l == 0 :
+        n_l = nt[ci]  # number of labels
+        n_p = i.sum()  # number of predictions
+        if n_p == 0 or n_l == 0:
             continue
-        fpc = (1-tp[i]).cumsum(0)
+
+        # Accumulate FPs and TPs
+        fpc = (1 - tp[i]).cumsum(0)
         tpc = tp[i].cumsum(0)
 
-        recall = tpc / (n_l+eps)
-        r[ci] = np.interp(-px,-conf[i],recall[:,0],left=0)
+        # Recall
+        recall = tpc / (n_l + eps)  # recall curve
+        r[ci] = np.interp(-px, -conf[i], recall[:, 0], left=0)  # negative x, xp because xp decreases
 
-        precision = tpc / (tpc + fpc)
-        p[ci] = np.interp(-px,-conf[i],precision[:,0],left=1)
+        # Precision
+        precision = tpc / (tpc + fpc)  # precision curve
+        p[ci] = np.interp(-px, -conf[i], precision[:, 0], left=1)  # p at pr_score
 
+        # AP from recall-precision curve
         for j in range(tp.shape[1]):
-            ap[ci,j],_,_ = compute_ap(recall[:,j],precision[:,j])
-    f1 = 2*p*r/(p + r + eps)
+            ap[ci, j], mpre, mrec = compute_ap(recall[:, j], precision[:, j])
+            
 
-    i = smooth(f1.mean(0),0.1).argmax()
-    p,r,f1 = p[:,i], r[:,i],f1[:,i]
-    tp = (r*nt).round()
-    fp = (tp/(p+eps)-tp).round()
-    return tp,fp,p,r,f1,ap,unique_classes.astype(int)
-
-
-def smooth(y, f=0.05):
-    """
-    mean filtering
-    input   y[n] 
-            f ratio
-    output  after filter value  [n] 
-
+    # Compute F1 (harmonic mean of precision and recall)
+    f1 = 2 * p * r / (p + r + eps)
+    names = [v for k, v in names.items() if k in unique_classes]  # list: only classes that have data
+    names = dict(enumerate(names))  # to dict
     
-    """
-    nf = round(len(y) * f * 2) // 2 + 1 
-    p = np.ones(nf // 2)  
-    yp = np.concatenate((p * y[0], y, p * y[-1]), 0)  
-    return np.convolve(yp, np.ones(nf) / nf, mode='valid')  
 
+    i = smooth(f1.mean(0), 0.1).argmax()  # max F1 index
+    p, r, f1 = p[:, i], r[:, i], f1[:, i]
+    tp = (r * nt).round()  # true positives
+    fp = (tp / (p + eps) - tp).round()  # false positives
+    return tp, fp, p, r, f1, ap, unique_classes.astype(int)
 
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves
+    # Arguments
+        recall:    The recall curve (list)
+        precision: The precision curve (list)
+    # Returns
+        Average precision, precision curve, recall curve
+    """
 
-def compute_ap(
-    recall, 
-    precision    
-):
-    """
-    input   recall [ ] 
-            precision [ ]
-    output  ap      
-            mpr     
-            mrec    
-    
-    """
+    # Append sentinel values to beginning and end
     mrec = np.concatenate(([0.0], recall, [1.0]))
     mpre = np.concatenate(([1.0], precision, [0.0]))
 
+    # Compute the precision envelope
     mpre = np.flip(np.maximum.accumulate(np.flip(mpre)))
+
+    # Integrate area under curve
     method = 'interp'  # methods: 'continuous', 'interp'
     if method == 'interp':
         x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
@@ -205,35 +196,172 @@ def compute_ap(
 
     return ap, mpre, mrec
 
+def smooth(y, f=0.05):
+    # Box filter of fraction f
+    nf = round(len(y) * f * 2) // 2 + 1  # number of filter elements (must be odd)
+    p = np.ones(nf // 2)  # ones padding
+    yp = np.concatenate((p * y[0], y, p * y[-1]), 0)  # y padded
+    return np.convolve(yp, np.ones(nf) / nf, mode='valid')  # y-smoothed
 
-
-
-
-def process_batch(
-        detections, 
-        labels, 
-        iouv
-        ):
+def process_batch(detections, labels, iouv):
     """
-    input   detections  [N,6]   x1 y1 x2 y2 conf class
-            labels      [M 5]   class x1 y1 x2 y2
-            iouv        [10]  0.5-0.95
-    output  correct     [ ,10]  0.5-0.95 bool
-       
+    Return correct prediction matrix
+    Arguments:
+        detections (array[N, 6]), x1, y1, x2, y2, conf, class
+        labels (array[M, 5]), class, x1, y1, x2, y2
+    Returns:
+        correct (array[N, 10]), for 10 IoU levels
     """
-    correct = np.zeros((detections.shape[0],iouv.shape[0])).astype(bool)
-    iou = box_iou(labels[:,1:],detections[:,:4])
-    correct_class = labels[:,0:1]==detections[:,5]
+    correct = np.zeros((detections.shape[0], iouv.shape[0])).astype(bool)
+    iou = box_iou(labels[:, 1:], detections[:, :4])
+    correct_class = labels[:, 0:1] == detections[:, 5]
     for i in range(len(iouv)):
-        x = torch.where((iou > iouv[i]) & correct_class) 
+        x = torch.where((iou >= iouv[i]) & correct_class)  # IoU > threshold and classes match
         if x[0].shape[0]:
-            matches = torch.cat((torch.stack(x,1),iou[x[0],x[1]][:,None]),1).cpu().numpy()
+            matches = torch.cat((torch.stack(x, 1), iou[x[0], x[1]][:, None]), 1).cpu().numpy()  # [label, detect, iou]
             if x[0].shape[0] > 1:
                 matches = matches[matches[:, 2].argsort()[::-1]]
                 matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
+                # matches = matches[matches[:, 2].argsort()[::-1]]
                 matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
-            correct[matches[:,1].astype(int),i] = True
-        return  torch.tensor(correct, dtype=torch.bool, device=iouv.device)  
+            correct[matches[:, 1].astype(int), i] = True
+    return torch.tensor(correct, dtype=torch.bool, device=iouv.device)
+
+
+
+
+
+# def ap_per_class(
+#         tp, 
+#         conf, 
+#         pred_cls, 
+#         target_cls,
+#         names = None, 
+#         eps=1e-16 
+#         ):
+#     """
+#         Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+
+#         input   tp          []
+#                 conf        []
+#                 pred_cls    []
+#                 target_cls  
+#                 names
+#                 eps             zuixiaozhi
+#         output  tp
+#                 fp
+#                 p
+#                 r
+#                 f1
+#                 ap
+
+
+#     """
+#     i = np.argsort(-conf)
+#     tp, conf, pred_cls = tp[i], conf[i],pred_cls[i]
+
+#     unique_classes,nt = np.unique(target_cls,return_counts=True)
+#     nc = unique_classes.shape[0]
+
+#     px, py = np.linspace(0,1,1000),[]
+#     ap,p,r = np.zeros((nc,tp.shape[1])),np.zeros((nc,1000)),np.zeros((nc,1000))
+#     for ci,c in enumerate(unique_classes):
+#         i = pred_cls == c
+#         n_l = nt[ci]
+#         n_p = i.sum()
+#         if n_p == 0 or n_l == 0 :
+#             continue
+#         fpc = (1-tp[i]).cumsum(0)
+#         tpc = tp[i].cumsum(0)
+
+#         recall = tpc / (n_l+eps)
+#         r[ci] = np.interp(-px,-conf[i],recall[:,0],left=0)
+
+#         precision = tpc / (tpc + fpc)
+#         p[ci] = np.interp(-px,-conf[i],precision[:,0],left=1)
+
+#         for j in range(tp.shape[1]):
+#             ap[ci,j],_,_ = compute_ap(recall[:,j],precision[:,j])
+#     f1 = 2*p*r/(p + r + eps)
+
+#     i = smooth(f1.mean(0),0.1).argmax()
+#     p,r,f1 = p[:,i], r[:,i],f1[:,i]
+#     tp = (r*nt).round()
+#     fp = (tp/(p+eps)-tp).round()
+#     return tp,fp,p,r,f1,ap,unique_classes.astype(int)
+
+
+# def smooth(y, f=0.05):
+#     """
+#     mean filtering
+#     input   y[n] 
+#             f ratio
+#     output  after filter value  [n] 
+
+    
+#     """
+#     nf = round(len(y) * f * 2) // 2 + 1 
+#     p = np.ones(nf // 2)  
+#     yp = np.concatenate((p * y[0], y, p * y[-1]), 0)  
+#     return np.convolve(yp, np.ones(nf) / nf, mode='valid')  
+
+
+
+# def compute_ap(
+#     recall, 
+#     precision    
+# ):
+#     """
+#     input   recall [ ] 
+#             precision [ ]
+#     output  ap      
+#             mpr     
+#             mrec    
+    
+#     """
+#     mrec = np.concatenate(([0.0], recall, [1.0]))
+#     mpre = np.concatenate(([1.0], precision, [0.0]))
+
+#     mpre = np.flip(np.maximum.accumulate(np.flip(mpre)))
+#     method = 'interp'  # methods: 'continuous', 'interp'
+#     if method == 'interp':
+#         x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
+#         ap = np.trapz(np.interp(x, mrec, mpre), x)  # integrate
+#     else:  # 'continuous'
+#         i = np.where(mrec[1:] != mrec[:-1])[0]  # points where x axis (recall) changes
+#         ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])  # area under curve
+
+#     return ap, mpre, mrec
+
+
+
+
+
+# def process_batch(
+#         detections, 
+#         labels, 
+#         iouv
+#         ):
+#     """
+#     input   detections  [N,6]   x1 y1 x2 y2 conf class
+#             labels      [M 5]   class x1 y1 x2 y2
+#             iouv        [10]  0.5-0.95
+#     output  correct     [ ,10]  0.5-0.95 bool
+       
+#     """
+#     correct = np.zeros((detections.shape[0],iouv.shape[0])).astype(bool)
+#     iou = box_iou(labels[:,1:],detections[:,:4])
+#     correct_class = labels[:,0:1]==detections[:,5]
+#     for i in range(len(iouv)):
+#         x = torch.where((iou > iouv[i]) & correct_class) 
+#         if x[0].shape[0]:
+#             matches = torch.cat((torch.stack(x,1),iou[x[0],x[1]][:,None]),1).cpu().numpy()
+#             if x[0].shape[0] > 1:
+#                 matches = matches[matches[:, 2].argsort()[::-1]]
+#                 matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
+#                 matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
+#             correct[matches[:,1].astype(int),i] = True
+#         return  torch.tensor(correct, dtype=torch.bool, device=iouv.device)  
 
 
 
