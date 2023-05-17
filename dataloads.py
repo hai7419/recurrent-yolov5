db8f33720ea6ee4c2c063e24d3d1b253f9e122f9
hai7419 <wangzhenxia18@163.com>
2023-05-17 14:37:25 +0800

    bfsbfb

--- dataloads.py
+++ dataloads.py
@@ -936,58 +936,117 @@ class yolodateset(Dataset):
             f = self.npy_files[i]
             if not f.exists():
                 np.save(f.as_posix(), cv2.imread(self.im_files[i]))
-    def laod_mosaic(self,index):
-        """
-            output  img [hwc]   type:numpy
-                    lables [class xyxy]  pixels
-        """
-        labels4 = []
-        size = self.im_size
-        hc,wc = (int(random.uniform(-x,2*size+x)) for x in self.mosaic_border)
-        indices = [index]+random.choices(self.indices,k=3)
+    
+    def load_mosaic(self, index):
+        # YOLOv5 4-mosaic loader. Loads 1 image + 3 random images into a 4-image mosaic
+        labels4, segments4 = [], []
+        s = self.img_size
+        yc, xc = (int(random.uniform(-x, 2 * s + x)) for x in self.mosaic_border)  # mosaic center x, y
+        indices = [index] + random.choices(self.indices, k=3)  # 3 additional image indices
         random.shuffle(indices)
-        for i,index in enumerate(indices):
-            img,_,(h,w) = self.load_img(index=index)
+        for i, index in enumerate(indices):
+            # Load image
+            img, _, (h, w) = self.load_image(index)
 
+            # place img in img4
             if i == 0:  # top left
-                img4 = np.full((size * 2, size * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
-                x1a, y1a, x2a, y2a = max(wc - w, 0), max(hc - h, 0), wc, hc  # xmin, ymin, xmax, ymax (large image)
+                img4 = np.full((s * 2, s * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
+                x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
                 x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
             elif i == 1:  # top right
-                x1a, y1a, x2a, y2a = wc, max(hc - h, 0), min(wc + w, size * 2), hc
+                x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
                 x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
             elif i == 2:  # bottom left
-                x1a, y1a, x2a, y2a = max(wc - w, 0), hc, wc, min(size * 2, hc + h)
+                x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
                 x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
             elif i == 3:  # bottom right
-                x1a, y1a, x2a, y2a = wc, hc, min(wc + w, size * 2), min(size * 2, hc + h)
+                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
                 x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
 
             img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
-            
             padw = x1a - x1b
             padh = y1a - y1b
 
-            labels = self.labels[index].copy()
-
+            # Labels
+            labels = self.labels[index].copy()#, self.segments[index].copy()
             if labels.size:
-                labels[:,1:] = self.xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  
-            
+                labels[:, 1:] = self.xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  # normalized xywh to pixel xyxy format
+                # segments = [xyn2xy(x, w, h, padw, padh) for x in segments]
             labels4.append(labels)
-        labels4 = np.concatenate(labels4,0)
-        np.clip(labels4[:,1:],0,2*size,out=labels4[:,1:])
+            # segments4.extend(segments)
+
+        # Concat/clip labels
+        labels4 = np.concatenate(labels4, 0)
+        for x in (labels4[:, 1:]):
+            np.clip(x, 0, 2 * s, out=x)  # clip when using random_perspective()
+        # img4, labels4 = replicate(img4, labels4)  # replicate
+
+        # Augment
+        # img4, labels4, segments4 = copy_paste(img4, labels4, segments4, p=self.hyp['copy_paste'])
+        img4, labels4 = randon_perspective(img4,
+                                           labels4,
+                                           segments4,
+                                           degrees=self.hyp['degrees'],
+                                           translate=self.hyp['translate'],
+                                           scale=self.hyp['scale'],
+                                           shear=self.hyp['shear'],
+                                           perspective=self.hyp['perspective'],
+                                           border=self.mosaic_border)  # border to remove
+
+        return img4, labels4
+    
+    # def laod_mosaic(self,index):
+    #     """
+    #         output  img [hwc]   type:numpy
+    #                 lables [class xyxy]  pixels
+    #     """
+    #     labels4 = []
+    #     size = self.im_size
+    #     hc,wc = (int(random.uniform(-x,2*size+x)) for x in self.mosaic_border)
+    #     indices = [index]+random.choices(self.indices,k=3)
+    #     random.shuffle(indices)
+    #     for i,index in enumerate(indices):
+    #         img,_,(h,w) = self.load_img(index=index)
+
+    #         if i == 0:  # top left
+    #             img4 = np.full((size * 2, size * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
+    #             x1a, y1a, x2a, y2a = max(wc - w, 0), max(hc - h, 0), wc, hc  # xmin, ymin, xmax, ymax (large image)
+    #             x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
+    #         elif i == 1:  # top right
+    #             x1a, y1a, x2a, y2a = wc, max(hc - h, 0), min(wc + w, size * 2), hc
+    #             x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
+    #         elif i == 2:  # bottom left
+    #             x1a, y1a, x2a, y2a = max(wc - w, 0), hc, wc, min(size * 2, hc + h)
+    #             x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
+    #         elif i == 3:  # bottom right
+    #             x1a, y1a, x2a, y2a = wc, hc, min(wc + w, size * 2), min(size * 2, hc + h)
+    #             x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
+
+    #         img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
+            
+    #         padw = x1a - x1b
+    #         padh = y1a - y1b
+
+    #         labels = self.labels[index].copy()
+
+    #         if labels.size:
+    #             labels[:,1:] = self.xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  
+            
+    #         labels4.append(labels)
+    #     labels4 = np.concatenate(labels4,0)
+    #     np.clip(labels4[:,1:],0,2*size,out=labels4[:,1:])
         
-        img4 , labels4 = randon_perspective(
-            im = img4,
-            targets=labels4,
-            degrees=self.hyp['degrees'],
-            translate=self.hyp['translate'],
-            scale=self.hyp['scale'],
-            shear=self.hyp['shear'],
-            perspective=self.hyp['perspective'],
-            border=self.mosaic_border
-        )
-        return img4,labels4
+    #     img4 , labels4 = randon_perspective(
+    #         im = img4,
+    #         targets=labels4,
+    #         degrees=self.hyp['degrees'],
+    #         translate=self.hyp['translate'],
+    #         scale=self.hyp['scale'],
+    #         shear=self.hyp['shear'],
+    #         perspective=self.hyp['perspective'],
+    #         border=self.mosaic_border
+    #     )
+    #     return img4,labels4
     def load_img(self,index):
         """
                 no pad
