cb7dcf4d33759997879c54cc03492bb130e2cec4
hai7419 <wangzhenxia18@163.com>
2023-05-19 16:06:40 +0800

    indexeer

--- dataloads.py
+++ dataloads.py
@@ -957,7 +957,7 @@ class yolodateset(Dataset):
         random.shuffle(indices)
         for i, index in enumerate(indices):
             # Load image
-            img, _, (h, w) = self.load_img(index=index) 
+            img, _, (h, w) = self.load_img(i=index) 
 
             # place img in img4
             if i == 0:  # top left
